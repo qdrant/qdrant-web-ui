845ee27715dd1bcc13f42a3ee508f553983fbaf9
chetankk <chetankhulage350@gmail.com>
2023-03-20 10:49:08 +0530

    header-sidebar-main-footer added with MUI

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,10 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import MainContent from "../MainContent/MainContent";
 import Footer from "../Footer/Footer";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 function Home() {
   return (
@@ -35,8 +24,6 @@ function Home() {
         </Grid>
       </Grid>
     </Box>
-
-    // <div className="Home">    </div>
   );
 }
 
