3e9713416766f8f1027c36f14a37c98e5ec2f4b2
Kartik Gupta <84975264+kartik-gupta-ij@users.noreply.github.com>
2023-04-02 02:12:11 +0530

    reverts the change

--- src/components/CodeEditorWindow/config/Theme.js
+++ src/components/CodeEditorWindow/config/Theme.js
@@ -1,12 +1,12 @@
 export const Theme = {
     base: 'vs',
     rules: [
-        { token: 'keyword', foreground: '#0000FF', fontStyle: 'bold' },
+        { token: 'keyword', foreground: '#0000FF' },
         { token: 'string.key', foreground: '#A31515' },
         { token: 'string.value', foreground: '#0451A5' },
         { token: 'number', foreground: '#098658' },
         { token: 'comment', foreground: '#008000'},
-        { token: 'variable', foreground: '#DC244C'},
+
         { token: 'string', foreground: '#A31515' },
     ],
     colors: {
