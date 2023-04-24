6fad50d0148994252b1b22349dbd6c08d24fa6c3
Carlos Ziegler <carlos.ziegler@gmail.com>
2023-04-24 22:35:00 +0200

    feat: add types, specs and assets

--- src/common/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-function setupAxios() {
-  if (process.env.NODE_ENV === "development") {
-    axios.defaults.baseURL = "http://localhost:6333";
-  }
-}
-
-export default setupAxios;
