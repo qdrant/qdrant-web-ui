7fd680c653ffe20b4dfeff683cafb47805176c89
generall <andrey@vasnetsov.com>
2023-06-28 20:07:27 +0200

    explicitly specify port in qdrant client

--- src/common/client.js
+++ src/common/client.js
@@ -4,15 +4,26 @@ import { QdrantClient } from '@qdrant/js-client-rest';
 
 export default function qdrantClient({ apiKey }) {
   let url;
+  let port = 6333;
   if (process.env.NODE_ENV === "development") {
     url = "http://localhost:6333";
   } else {
     url = window.location.href;
+    if (window.location.port) {
+      port = window.location.port;
+    } else {
+      if (window.location.protocol === "https:") {
+        port = 443;
+      } else {
+        port = 80;
+      }
+    }
   }
 
   let options = {
     url,
-    apiKey
+    apiKey,
+    port,
   };
 
   return new QdrantClient(options)
