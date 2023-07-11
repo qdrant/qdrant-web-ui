f0f8e71a8a5c12916f357fb7ebf6dc27489ac214
trean <trean.mi@gmail.com>
2023-07-11 21:21:49 +0200

    uppy

--- src/components/Snapshots/SnapshotUploadForm.jsx
+++ src/components/Snapshots/SnapshotUploadForm.jsx
@@ -1,75 +1,157 @@
-import React, { useState } from "react";
+import React, { lazy, useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import TextField from "@mui/material/TextField";
-import { Button, Card, CardActions, CardContent } from "@mui/material";
-import { Dropzone } from "../Forms/Dropzone";
+import {
+  StepContent,
+  Stepper,
+  Typography,
+  Box,
+  StepLabel,
+  Step,
+  Paper,
+  Button,
+} from "@mui/material";
 import { useClient } from "../../context/client-context";
+import { Uppy } from "@uppy/core";
+import DragDrop from "@uppy/react/lib/DragDrop";
+import ProgressBar from "@uppy/react/lib/ProgressBar";
+import XHR from "@uppy/xhr-upload";
+
+import "@uppy/core/dist/style.min.css";
+import "@uppy/drag-drop/dist/style.min.css";
+import "@uppy/progress-bar/dist/style.min.css";
 
 export const SnapshotUploadForm = ({ onSubmit, sx }) => {
   const { client: qdrantClient } = useClient();
   const [collectionName, setCollectionName] = useState("");
-  const [files, setFiles] = useState([]);
   const [formError, setFormError] = useState(false);
+  const [fileAdded, setFileAdded] = useState(false);
+  const COLLECTION_NAME_LENGTH = 4;
 
-  const handleDrop = (acceptedFiles) => {
-    setFiles(acceptedFiles);
+  /**
+   * Get the endpoint URL for uploading a snapshot
+   * @type {function(): string}
+   */
+  const getEndpointUrl = useCallback(() => {
+    return new URL(`/collections/${collectionName}/snapshots/upload`,
+      qdrantClient._restUri).href;
+  }, [collectionName, qdrantClient]);
+
+  const uppy = new Uppy({
+    restrictions: {
+      // maxFileSize: 1000000, // todo: what is the max file size?
+      maxNumberOfFiles: 1,
+      minNumberOfFiles: 1,
+      allowedFileTypes: ["application/x-tar", ".snapshot"],
+    },
+    autoProceed: true,
+    debug: true, // todo
+  });
+
+  uppy.use(XHR, {
+    endpoint: getEndpointUrl(),
+    formData: true,
+    fieldName: "snapshot",
+  });
+
+  uppy.on("file-added", (file) => {
+    setFileAdded(true);
+  });
+
+  uppy.on("complete", (result) => {
+    console.log("Upload result:", result);
+    handleFinish();
+    onSubmit();
+  });
+
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
-  const handleSubmit = () => {
-    console.log(files);
-    console.log(qdrantClient);
-    console.log(qdrantClient._restUri);
-    const formData = new FormData();
-    formData.append("file", files[0]);
-    const url = new URL(`/collections/${collectionName}/snapshots/upload`,
-      qdrantClient._restUri);
-    const headers = new Headers();
-    headers.append("Content-Type", "multipart/form-data");
-    fetch(url, {
-      method: "POST",
-      headers,
-      body: formData,
-    }).then((response) => {
-      console.log("response:");
-      console.log(response);
-    }).catch(error => console.error(error));
-    if (onSubmit) onSubmit();
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
+
+  const handleFinish = () => {
+    setActiveStep(3);
+  }
 
   return (
-    <Card sx={{ ...sx }}>
-      <CardContent>
+    <Box>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {/*Step 1 start - enter a collection name*/}
+        <Step key={"Step 1 - enter a collection name"}>
+          <StepLabel>
+            Step 1 - Enter a collection name
+          </StepLabel>
+          <StepContent>
+            <Typography mb={2}>Can be new or existing</Typography>
+            <Box sx={{ mb: 2 }}>
+              <TextField
+                error={formError}
+                id="collection-name"
+                label="Collection Name"
+                value={collectionName}
+                helperText={formError ? "Collection name is required" : " "}
+                onChange={(event) => {
+                  // todo: move
+                  setCollectionName(event.target.value);
+                  event.target.value.length < COLLECTION_NAME_LENGTH ?
+                    setFormError(true) :
+                    setFormError(false);
+                }}
+                fullWidth={true}
+              />
+            </Box>
+            <Box sx={{ mb: 2 }}>
+              <Button
+                variant="contained"
+                onClick={handleNext}
+                sx={{ mt: 1, mr: 1 }}
+                disabled={!collectionName || formError}
+              >
+                Continue
+              </Button>
+            </Box>
+          </StepContent>
+        </Step>
+        {/*Step 1 end - enter a collection name*/}
 
-        <TextField
-          error={formError}
-          id="collection-name"
-          label="Collection Name"
-          value={collectionName}
-          helperText={formError ? "Invalide value" : " "}
-          onChange={(event) => {
-            // todo: move
-            setCollectionName(event.target.value);
-            collectionName.length < 4 ?
-              setFormError(true) :
-              setFormError(false);
-          }}
-        />
-        <Dropzone
-          onDrop={handleDrop}
-          options={{
-            accept: {
-              "application/x-tar": [".snapshot"],
-            },
-            maxFiles: 1,
-          }}
-        />
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" onClick={handleSubmit}>
-          Upload Snapshot
-        </Button>
-      </CardActions>
-    </Card>
+        {/*Step 2 start - upload a snapshot file*/}
+        <Step key={"Step 2 - upload a snapshot file"}>
+          <StepLabel>
+            Step 2 - Upload a snapshot file
+          </StepLabel>
+          <StepContent>
+            <Box sx={{ mb: 2 }}>
+              <div>
+              {/*Here we have a drag and drop area*/}
+              <DragDrop uppy={uppy}/>
+              <ProgressBar uppy={uppy}/>
+              </div>
+            </Box>
+            <Box mb={2}>
+              <Button
+                variant="contained"
+                onClick={handleBack}
+                sx={{ mt: 1, mr: 1 }}
+                disabled={fileAdded}
+              >
+                Back
+              </Button>
+            </Box>
+          </StepContent>
+        </Step>
+        {/*Step 2 end - upload a snapshot file*/}
+      </Stepper>
+      {activeStep === 3 && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished 🎉</Typography>
+        </Paper>
+      )}
+    </Box>
   );
 };
 
--- src/components/Snapshots/SnapshotsUpload.jsx
+++ src/components/Snapshots/SnapshotsUpload.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Button } from "@mui/material";
+import { Box, Tooltip } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import { SnapshotUploadForm } from "./SnapshotUploadForm";
+import { UploadFile } from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export const SnapshotsUpload = ({ sx }) => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
   const [open, setOpen] = React.useState(false);
   const handleUploadClick = () => {
     setOpen(true);
   };
 
+  const handleUpload = () => {
+    setTimeout(() => {
+      setOpen(false);
+    }, 1000);
+  };
+
   return (
     <Box sx={{ ...sx }}>
-      <Button variant="contained" onClick={handleUploadClick}>Upload
-        Snapshot</Button>
-      <Dialog open={open} onClose={() => setOpen(false)}
-              aria-labelledby="File upload dialog"
-              aria-describedby="File upload dialog"
+      <Tooltip title={"Upload snapshot"} placement="left">
+        <IconButton variant="contained" onClick={handleUploadClick}
+                    color={"primary"}><UploadFile/></IconButton>
+      </Tooltip>
+      <Dialog
+        fullScreen={fullScreen} fullWidth={true} maxWidth={"md"}
+        open={open} onClose={() => setOpen(false)}
+        aria-labelledby="Snapshot upload dialog"
+        aria-describedby="Snapshot upload dialog"
       >
-        <SnapshotUploadForm onSubmit={() => setOpen(false)}/>
+        <DialogTitle>Upload A Snapshot</DialogTitle>
+        <DialogContent>
+          <SnapshotUploadForm onSubmit={handleUpload}/>
+        </DialogContent>
       </Dialog>
     </Box>
   );
