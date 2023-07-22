6bc714b7cac99a50f8358781d7d800ffe4bea844
Kartik Gupta <kartik-gupta-ij@Kartiks-MacBook-Air.local>
2023-07-22 18:57:25 +0530

    initial setup graph

--- src/components/VisualizeEditorWindow/index.jsx
+++ src/components/VisualizeEditorWindow/index.jsx
@@ -1,22 +1,51 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect } from "react";
 import EditorCommon from "../EditorCommon";
+import Chart from 'chart.js/auto';
+
 
 const VisualizeEditorWindow = ({ scrollResult }) => {
 
   const [editorData, setEditorData] = React.useState("{}");
+// write a demo chart fuction in scatter plot
+  useEffect(() => {
+
+    return () => {
+      
+    }
+  }, []);
+
   useEffect(() => {
     const worker = new Worker(new URL("./worker.js", import.meta.url), {
       type: "module",
     });
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+      type: 'scatter',
+      data: {
+        datasets: [{
+          label: 'Scatter Dataset',
+          data: []
+        }]
+      },
+      options: {
+        responsive: true,
+        fullsize: true,
+
+      }
+    });
 
     worker.onmessage = (m) => {
+      console.log("message from worker", m.data);
       setEditorData(m.data);
+      myChart.data.datasets[0].data = m.data;
+      myChart.update();
     };
 
     worker.postMessage(scrollResult);
 
     return () => {
+      myChart.destroy();
       worker.terminate();
     };
   }, [scrollResult]);
@@ -34,20 +63,7 @@ const VisualizeEditorWindow = ({ scrollResult }) => {
   }
 
   return (
-    <EditorCommon
-      language="json"
-      theme={"custom-language-theme"}
-      value={formatJSON(editorData)}
-      options={{
-        scrollBeyondLastLine: false,
-        fontSize: 12,
-        wordWrap: "on",
-        minimap: { enabled: false },
-        automaticLayout: true,
-        readOnly: true,
-        mouseWheelZoom: true,
-      }}
-    />
+    <canvas id="myChart" height={"100%"} width={"100%"}></canvas>
   );
 };
 export default VisualizeEditorWindow;
