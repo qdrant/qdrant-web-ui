b44d9b9f5530e02a777d7b0bbf339711063555cc
generall <andrey@vasnetsov.com>
2023-07-31 14:30:55 +0200

    fix release ci

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   plugins: [
     reactRefresh(),
