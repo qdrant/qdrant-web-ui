974488750b150255aadcdad0de611c6e87a8312a
trean <trean.mi@gmail.com>
2023-10-16 15:49:20 +0200

    tutorila pages

--- src/components/InteractiveTutorial/TutorialSubpages.jsx
+++ src/components/InteractiveTutorial/TutorialSubpages.jsx
@@ -1,7 +1,6 @@
 import * as Index from './MdxPages/Index.mdx';
 import * as Quickstart from './MdxPages/Quickstart.mdx';
 import * as FilteringClauses from './MdxPages/FilteringClauses.mdx';
-import * as FilteringConditions from './MdxPages/FilteringConditions.mdx';
 
 /**
  * MDX page object (Index etc.) contains:
@@ -14,7 +13,6 @@ export const tutorialIndexPage = Index;
 const tutorialSubPages = [
   ['quickstart', Quickstart],
   ['filtering-clauses', FilteringClauses],
-  ['filtering-conditions', FilteringConditions],
 ];
 
 export { tutorialSubPages };
