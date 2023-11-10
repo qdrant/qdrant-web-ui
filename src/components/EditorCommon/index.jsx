efd8cc068a306009def0d65036d05edc55c0f91e
trean <trean.mi@gmail.com>
2023-11-10 23:10:20 +0100

    added monaco editor loader to use the editor without cdn

--- src/components/EditorCommon/index.jsx
+++ src/components/EditorCommon/index.jsx
@@ -5,6 +5,23 @@ import { getEditorTheme } from './config/theme';
 import { langConfig, Rules } from './config/Rules';
 import { useTheme } from '@mui/material/styles';
 import { useWindowResize } from '../../hooks/windowHooks';
+import * as monaco from 'monaco-editor';
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import { loader } from '@monaco-editor/react';
+
+
+
+window.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'json') {
+      return new JsonWorker();
+    }
+    return new EditorWorker();
+  },};
+
+
+loader.config({monaco});
 
 const EditorCommon = ({ beforeMount, ...props }) => {
   const monacoRef = useRef(null);
