73d8d06dee3a7a64296fdbee48c1e14af4ebecca
generall <andrey@vasnetsov.com>
2023-11-24 13:25:59 +0100

    remove unused language workers (-6mb bundle size)

--- src/components/EditorCommon/index.jsx
+++ src/components/EditorCommon/index.jsx
@@ -8,9 +8,6 @@ import { useWindowResize } from '../../hooks/windowHooks';
 import * as monaco from 'monaco-editor';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 import { loader } from '@monaco-editor/react';
 
 window.MonacoEnvironment = {
@@ -18,15 +15,7 @@ window.MonacoEnvironment = {
     if (label === 'json') {
       return new JsonWorker();
     }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new CssWorker();
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new HtmlWorker();
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new TsWorker();
-    }
+    // Additional language-server support should be added here
     return new EditorWorker();
   },
 };
