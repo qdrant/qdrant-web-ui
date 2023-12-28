07df27e66afa2a39b6469abbda49b520f5973d3b
trean <trean.mi@gmail.com>
2023-12-28 18:48:25 +0000

    show messages from ApiError

--- src/components/ToastNotifications/ErrorNotifier.jsx
+++ src/components/ToastNotifications/ErrorNotifier.jsx
@@ -3,23 +3,21 @@ import PropTypes from 'prop-types';
 import { useSnackbar } from 'notistack';
 import { getSnackbarOptions } from '../Common/utils/snackbarOptions';
 
-export const ErrorNotifier = ({ message }) => {
+export const ErrorNotifier = ({ message = 'Something went wrong', callback }) => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const errorSnackbarOptions = getSnackbarOptions('error', closeSnackbar, 6000);
 
   useEffect(() => {
     enqueueSnackbar(message, errorSnackbarOptions);
+    typeof callback === 'function' && callback();
   }, [enqueueSnackbar, errorSnackbarOptions, message]);
 
   return null;
 };
 
-ErrorNotifier.defaultProps = {
-  message: 'We are sorry, but something went wrong. Please, try again later.',
-};
-
 ErrorNotifier.propTypes = {
   message: PropTypes.string,
+  callback: PropTypes.func,
 };
 
 export default ErrorNotifier;
