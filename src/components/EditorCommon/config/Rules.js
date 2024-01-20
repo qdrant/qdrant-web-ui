aefdf61f3ecc91c113564f39ce5976ea0e6c790c
Snehil Shah <snehilshah.989@gmail.com>
2024-01-21 04:48:20 +0530

    Format Source Code
    Signed-off-by: Snehil Shah <snehilshah.989@gmail.com>

--- src/components/EditorCommon/config/Rules.js
+++ src/components/EditorCommon/config/Rules.js
@@ -4,7 +4,7 @@ const Method = ['POST', 'GET', 'PUT', 'DELETE', 'PATCH', 'HEAD'];
 
 const response = await fetch(import.meta.env.BASE_URL + './openapi.json');
 const openapi = await response.json();
-const DOCS_BASE_URL = 'https://qdrant.github.io/qdrant/redoc/index.html#tag/'
+const DOCS_BASE_URL = 'https://qdrant.github.io/qdrant/redoc/index.html#tag/';
 
 const apiDocs = new OpenapiDocs(DOCS_BASE_URL, openapi);
 
