0d33f562029b6f86fa2469c5582c38ab1a880081
trean <trean.mi@gmail.com>
2024-01-30 13:15:26 +0100

    full screen img on the point view

--- src/components/Points/PointImage.jsx
+++ src/components/Points/PointImage.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { CardMedia, Grid } from '@mui/material';
+import { Box, CardMedia, Grid, Modal, Typography } from '@mui/material';
 
 function PointImage({ data, sx }) {
+  const [fullScreenImg, setFullScreenImg] = useState(null);
   const renderImages = () => {
     const images = [];
 
@@ -40,6 +41,7 @@ function PointImage({ data, sx }) {
               key={key}
               image={data[key]}
               alt={data[key]}
+              onClick={() => setFullScreenImg(data[key])}
             />
           );
         }
@@ -58,6 +60,54 @@ function PointImage({ data, sx }) {
   return (
     <Grid item xs={3} display="grid" justifyContent={'center'}>
       {images}
+      <Modal
+        open={!!fullScreenImg}
+        onClose={() => setFullScreenImg(null)}
+        componentsProps={{
+          backdrop: {
+            sx: { cursor: 'pointer' },
+            title: 'Click to close',
+          },
+        }}
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+            maxWidth: '100%',
+            maxHeight: '100%',
+            '&:focus': { outline: 'none' },
+          }}
+        >
+          <Typography
+            variant="h6"
+            component="p"
+            onClick={() => setFullScreenImg(null)}
+            sx={{
+              position: 'absolute',
+              top: '-60px',
+              right: 0,
+              padding: 1,
+              cursor: 'pointer',
+              color: 'white',
+              backgroundColor: 'rgba(0,0,0,0.5)',
+              borderRadius: '5px',
+            }}
+          >
+            Close [ESC]
+          </Typography>
+          <img
+            src={fullScreenImg}
+            alt={fullScreenImg}
+            style={{
+              maxWidth: '100%',
+              maxHeight: '100%',
+            }}
+          />
+        </Box>
+      </Modal>
     </Grid>
   );
 }
