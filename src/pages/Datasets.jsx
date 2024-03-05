56df34b6d72cfea064341916986066103326b79e
Kartik Gupta <84975264+kartik-gupta-ij@users.noreply.github.com>
2024-03-05 22:20:16 +0530

    Add Snapshots for Demo Collections in tutorial page (#153)
    * Add Datasets page and related components
    
    * Update undici package to version 5.28.3
    
    * Refactor API call in Datasets.jsx

---
+++ src/pages/Datasets.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { CenteredFrame } from '../components/Common/CenteredFrame';
+import { Grid, TableContainer, Typography } from '@mui/material';
+import { TableBodyWithGaps, TableWithGaps } from '../components/Common/TableWithGaps';
+import { DatasetsHeader } from '../components/Datasets/DatasetsTableHeader';
+import { DatasetsTableRow } from '../components/Datasets/DatasetsTableRow';
+import { useClient } from '../context/client-context';
+import { useSnackbar } from 'notistack';
+import { getSnackbarOptions } from '../components/Common/utils/snackbarOptions';
+
+function Datasets() {
+  const [datasets, setDatasets] = useState([]);
+  const { client: qdrantClient } = useClient();
+  const [isLoading, setIsLoading] = useState(false);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const errorSnackbarOptions = getSnackbarOptions('error', closeSnackbar);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://snapshots.qdrant.io/manifest.json');
+        const responseJson = await response.json();
+        const datasets = responseJson.map((dataset) => {
+          return {
+            name: dataset.name,
+            fileName: dataset.file_name,
+            size: dataset.size,
+            vectors: dataset.vectors,
+            vectorCount: dataset.vector_count,
+            description: dataset.description,
+          };
+        });
+        setDatasets(datasets);
+      } catch (error) {
+        enqueueSnackbar(error.message, errorSnackbarOptions);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const importDataset = async (fileName, collectionName, setImporting, importing) => {
+    if (importing) {
+      enqueueSnackbar('Importing in progress', errorSnackbarOptions);
+      return;
+    } else if (!collectionName) {
+      enqueueSnackbar('Collection name is required', errorSnackbarOptions);
+      return;
+    } else {
+      setImporting(true);
+
+      try {
+        const response = await qdrantClient.recoverSnapshot(collectionName, {
+          location: `https://snapshots.qdrant.io/${fileName}`,
+        });
+        console.log(response);
+        enqueueSnackbar('Snapshot successfully imported', getSnackbarOptions('success', closeSnackbar, 2000));
+      } catch (e) {
+        enqueueSnackbar(e.message, errorSnackbarOptions);
+      } finally {
+        setImporting(false);
+      }
+    }
+  };
+
+  const tableRows = datasets.map((dataset) => (
+    <DatasetsTableRow key={dataset.name} dataset={dataset} importDataset={importDataset} />
+  ));
+
+  return (
+    <>
+      <CenteredFrame>
+        <Grid container maxWidth={'xl'} spacing={3}>
+          <Grid xs={12} item>
+            <Typography variant="h4">Datasets</Typography>
+          </Grid>
+          {isLoading && <div>Loading...</div>}
+          {!isLoading && datasets?.length === 0 && <div>No datasets found</div>}
+          {!isLoading && datasets?.length > 0 && (
+            <Grid item xs={12}>
+              <TableContainer>
+                <TableWithGaps aria-label="simple table">
+                  <DatasetsHeader
+                    headers={[
+                      'Dataset Name',
+                      'Datasets size',
+                      'Vectors Configuration (Name, Size, Distance, Model)',
+                      'Vectors count',
+                      'Import',
+                    ]}
+                  />
+
+                  <TableBodyWithGaps>{tableRows}</TableBodyWithGaps>
+                </TableWithGaps>
+              </TableContainer>
+            </Grid>
+          )}
+        </Grid>
+      </CenteredFrame>
+    </>
+  );
+}
+
+export default Datasets;
