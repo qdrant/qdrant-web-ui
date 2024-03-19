c9bfe007526a4d3ad49bdee15b70451447696fff
kaijietti <43436010+kaijietti@users.noreply.github.com>
2024-03-20 02:37:46 +0800

    feat: add diff dialog to review changes before saving payload (#169)
    * feat: add diff dialog to review changes before saving payload
    
    * chore: format and fix audit
    
    * feat: add dark mode

--- src/components/Points/PayloadEditor.jsx
+++ src/components/Points/PayloadEditor.jsx
@@ -7,6 +7,8 @@ import { Button, Dialog, DialogActions, DialogTitle, DialogContent } from '@mui/
 import EditorCommon from '../EditorCommon';
 import { getSnackbarOptions } from '../Common/utils/snackbarOptions';
 import { bigIntJSON } from '../../common/bigIntJSON';
+import ReactDiffViewer from 'react-diff-viewer-continued';
+import { useTheme } from '@mui/material/styles';
 
 export const PayloadEditor = memo(({ collectionName, point, open, onClose, onSave, setLoading }) => {
   const { client: qdrantClient } = useClient();
@@ -14,6 +16,8 @@ export const PayloadEditor = memo(({ collectionName, point, open, onClose, onSav
   const errorSnackbarOptions = getSnackbarOptions('error', closeSnackbar, 6000);
   const successSnackbarOptions = getSnackbarOptions('success', closeSnackbar, 2000);
   const [payload, setPayload] = useState(() => bigIntJSON.stringify(point.payload, null, 2));
+  const [showDiff, setShowDiff] = useState(false);
+  const theme = useTheme();
 
   const savePayload = async (collectionName, options) => {
     if (Object.keys(point.payload).length !== 0) {
@@ -27,6 +31,24 @@ export const PayloadEditor = memo(({ collectionName, point, open, onClose, onSav
   };
 
   const handleSave = () => {
+    let payloadToSave;
+    try {
+      payloadToSave = bigIntJSON.parse(payload);
+    } catch (e) {
+      enqueueSnackbar(e.message, errorSnackbarOptions);
+      return;
+    }
+
+    // do nothing if payload is not changed
+    if (isEqual(point.payload, payloadToSave)) {
+      onClose();
+      return;
+    }
+
+    setShowDiff(true);
+  };
+
+  const handleConfirmSave = () => {
     let payloadToSave;
     try {
       payloadToSave = bigIntJSON.parse(payload);
@@ -65,52 +87,86 @@ export const PayloadEditor = memo(({ collectionName, point, open, onClose, onSav
       .finally(() => {
         // stop loading state and show success message
         setLoading(false);
+        setShowDiff(false);
       });
 
     onClose();
   };
 
   return (
-    <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
-      <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <>Edit payload for point {point.id}</>
-      </DialogTitle>
-      <DialogContent sx={{ pb: 1 }}>
-        <EditorCommon
-          height="50vh"
-          language="json"
-          value={bigIntJSON.stringify(point.payload, null, 2)}
-          onChange={handleChange}
-          options={{
-            scrollBeyondLastLine: false,
-            fontSize: 12,
-            wordWrap: 'on',
-            minimap: { enabled: false },
-            automaticLayout: true,
-            quickSuggestions: {
-              other: false,
-              comments: false,
-              strings: false,
-            },
-            parameterHints: {
-              enabled: false,
-            },
-            suggestOnTriggerCharacters: false,
-            acceptSuggestionOnEnter: 'off',
-            tabCompletion: 'off',
-            wordBasedSuggestions: false,
-          }}
-        />
-      </DialogContent>
-      <DialogActions sx={{ pb: 2, px: 3 }}>
-        <Button onClick={onClose} color="error" variant="outlined" sx={{ mr: 1 }}>
-          Cancel
-        </Button>
-        <Button onClick={handleSave} color="success" variant="outlined">
-          Save
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
+        <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <>Edit payload for point {point.id}</>
+        </DialogTitle>
+        <DialogContent sx={{ pb: 1 }}>
+          <EditorCommon
+            height="50vh"
+            language="json"
+            value={bigIntJSON.stringify(point.payload, null, 2)}
+            onChange={handleChange}
+            options={{
+              scrollBeyondLastLine: false,
+              fontSize: 12,
+              wordWrap: 'on',
+              minimap: { enabled: false },
+              automaticLayout: true,
+              quickSuggestions: {
+                other: false,
+                comments: false,
+                strings: false,
+              },
+              parameterHints: {
+                enabled: false,
+              },
+              suggestOnTriggerCharacters: false,
+              acceptSuggestionOnEnter: 'off',
+              tabCompletion: 'off',
+              wordBasedSuggestions: false,
+            }}
+          />
+        </DialogContent>
+        <DialogActions sx={{ pb: 2, px: 3 }}>
+          <Button onClick={onClose} color="error" variant="outlined" sx={{ mr: 1 }}>
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="success" variant="outlined">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={showDiff} onClose={() => setShowDiff(false)} fullWidth maxWidth="lg">
+        <DialogTitle>
+          <>Confirm to save payload changes</>
+        </DialogTitle>
+        <DialogContent>
+          <ReactDiffViewer
+            oldValue={bigIntJSON.stringify(point.payload, null, 2)}
+            newValue={payload}
+            splitView={true}
+            useDarkTheme={theme.palette.mode === 'dark'}
+            styles={{
+              diffContainer: {
+                fontSize: '12px',
+              },
+              wordDiff: {
+                padding: '0px',
+                wordBreak: 'normal',
+              },
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setShowDiff(false)} color="error" variant="outlined" sx={{ mr: 1 }}>
+            Cancel
+          </Button>
+          <Button onClick={handleConfirmSave} color="success" variant="outlined">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 });
 
