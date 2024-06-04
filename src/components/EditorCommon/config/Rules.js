586903d347b45b796ba00f786085f26add13fe6f
kartik-gupta-ij <kartikgupta7267@gmail.com>
2024-06-05 03:17:27 +0530

    fmt

--- src/components/EditorCommon/config/Rules.js
+++ src/components/EditorCommon/config/Rules.js
@@ -30,8 +30,8 @@ export const Rules = {
 
 export const langConfig = {
   comments: {
-    lineComment: "//",
-    blockComment: [ "/*", "*/" ]
+    lineComment: '//',
+    blockComment: ['/*', '*/'],
   },
   surroundingPairs: [
     { open: '{', close: '}' },
