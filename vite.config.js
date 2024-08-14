7b39e83e91ed1021236d6f882e1ff9aba590a928
J16N <jishan2001bhattacharya@gmail.com>
2024-08-14 13:31:23 +0530

    Add wasm support.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,25 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import eslintPlugin from 'vite-plugin-eslint';
-import {rehypeMetaAsAttributes} from "./src/lib/rehype-meta-as-attributes";
+import { rehypeMetaAsAttributes } from "./src/lib/rehype-meta-as-attributes";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import path from 'path';
+
+// this is required for wasm rayon to work
+const viteServerConfig = {
+  name: "log-request-middleware",
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET");
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      next();
+    });
+  }
+};
+
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
@@ -15,7 +33,13 @@ export default defineConfig(async () => {
     build: {
       outDir: 'dist',
     },
+    optimizeDeps: {
+      exclude: ['wasm-dist-bhtsne'],
+    },
     plugins: [
+      viteServerConfig,
+      wasm(),
+      topLevelAwait(),
       reactRefresh(),
       svgrPlugin({
         svgrOptions: {
@@ -41,6 +65,11 @@ export default defineConfig(async () => {
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./src/setupTests.js'],
+    },
+    resolve: {
+      alias: {
+        "examples": path.resolve(__dirname, "./examples"),
+      }
     },
   }
 });
