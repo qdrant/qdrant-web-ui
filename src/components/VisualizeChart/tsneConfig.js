1707de601461b5c57989b50f4807cf0f2d471557
J16N <jishan2001bhattacharya@gmail.com>
2024-09-04 21:35:29 +0530

    Comment out unused parameters.

--- src/components/VisualizeChart/tsneConfig.js
+++ src/components/VisualizeChart/tsneConfig.js
@@ -1,10 +1,10 @@
 export const tsneConfig = {
-    perplexity: 30,
-    learning_rate: 150,
+    perplexity: 30.0,
+    learning_rate: 150.0,
     theta: 0.5,
-    momentum: 0.5,
-    final_momentum: 0.8,
-    momentum_switch_epoch: 250,
-    stop_lying_epoch: 250,
-    embedding_dim: 2
+    // momentum: 0.5,
+    // final_momentum: 0.8,
+    // momentum_switch_epoch: 250,
+    // stop_lying_epoch: 250,
+    // embedding_dim: 2
 };
