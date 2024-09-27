b37fd1ee855c4293cf3d9ae2297c2d3ac7b026fe
kartik-gupta-ij <kartikgupta7267@gmail.com>
2024-09-27 11:22:52 +0530

    refactor: Update CreateCollectionForm and VectorConfig components
    - Refactor CreateCollectionForm component to handle sparse vectors and multivector configuration.
    - Update VectorConfig component to include options for sparse vectors and multivector configuration.
    - Update CreateCollection index component to set maxWidth to 'lg' in the Dialog.

--- src/components/CreateCollection/VectorConfig.jsx
+++ src/components/CreateCollection/VectorConfig.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   Button,
@@ -11,16 +11,20 @@ import {
   FormHelperText,
   IconButton,
   Tooltip,
+  FormControlLabel,
+  Checkbox,
 } from '@mui/material';
 import PropTypes from 'prop-types';
-import { AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
-
-const distancesOptions = ['Cosine', 'Euclid', 'Dot', 'Manhattan'];
+import { AddCircleOutline, DeleteOutline } from '@mui/icons-material';
 
 const VectorRow = ({ vectors, index, setVectors, errors, length, setErrors }) => {
+  const [distancesOptions, setDistancesOptions] = useState([]);
   const handleAddVector = () => {
-    setVectors([...vectors, { dimension: '', distance: '', name: '' }]);
-    setErrors([...errors, { dimension: '', distance: '' }]); // Add corresponding error object
+    setVectors([
+      ...vectors,
+      { dimension: '', distance: '', name: '', multivector_config: null, sparse_vectors: false },
+    ]);
+    setErrors([...errors, { dimension: '', distance: '', name: '' }]);
   };
 
   const handleRemoveVector = (index) => {
@@ -49,50 +53,110 @@ const VectorRow = ({ vectors, index, setVectors, errors, length, setErrors }) =>
     validate(index, field, value);
   };
 
+  const handleVectorConfigChange = (index, field, value) => {
+    if (value) {
+      const newVectors = [...vectors];
+
+      field === 'multivector_config'
+        ? (newVectors[index][field] = { comparator: 'max_sim' })
+        : (newVectors[index][field] = true);
+      field === 'multivector_config'
+        ? (newVectors[index].sparse_vectors = false)
+        : (newVectors[index].multivector_config = null);
+
+      setVectors(newVectors);
+    } else {
+      const newVectors = [...vectors];
+      newVectors[index][field] = null;
+      setVectors(newVectors);
+    }
+  };
+
+  useEffect(() => {
+    const getDistanceOptions = async () => {
+      const response = await fetch(import.meta.env.BASE_URL + './openapi.json');
+      const openapi = await response.json();
+      setDistancesOptions(openapi.components.schemas.Distance.enum);
+    };
+    getDistanceOptions();
+  }, []);
+
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-      <FormControl sx={{ mr: 2, width: '150px' }} error={!!errors[index].distance}>
-        <InputLabel>Distance</InputLabel>
-        <Select
-          value={vectors[index].distance}
-          onChange={(e) => handleVectorChange(index, 'distance', e.target.value)}
-          label="Distance"
-        >
-          {distancesOptions.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </Select>
-        <FormHelperText>{errors[index].distance}</FormHelperText>
-      </FormControl>
-      <TextField
-        label="Dimension"
-        value={vectors[index].dimension}
-        onChange={(e) => handleVectorChange(index, 'dimension', e.target.value)}
-        error={!!errors[index].dimension}
-        helperText={errors[index].dimension}
-        sx={{ mr: 2 }}
-      />
-      {length > 1 && (
+      {!vectors[index].sparse_vectors && (
         <>
+          <FormControl sx={{ mr: 2, width: '150px' }} error={!!errors[index].distance}>
+            <InputLabel>Distance</InputLabel>
+            <Select
+              value={vectors[index].distance}
+              onChange={(e) => handleVectorChange(index, 'distance', e.target.value)}
+              label="Distance"
+            >
+              {distancesOptions.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>{errors[index].distance}</FormHelperText>
+          </FormControl>
           <TextField
-            label="Name"
-            value={vectors[index].name}
-            onChange={(e) => handleVectorChange(index, 'name', e.target.value)}
-            error={!!errors[index].name}
-            helperText={errors[index].name}
+            label="Dimension"
+            value={vectors[index].dimension}
+            onChange={(e) => handleVectorChange(index, 'dimension', e.target.value)}
+            error={!!errors[index].dimension}
+            helperText={errors[index].dimension}
             sx={{ mr: 2 }}
           />
-          <Tooltip title="Remove vector" placement="top" sx={{ mr: 1 }}>
-            <IconButton onClick={() => handleRemoveVector(index)} color="error">
-              <RemoveCircleOutline />
-            </IconButton>
-          </Tooltip>
         </>
       )}
+
+      {(length > 1 || vectors[index].sparse_vectors) && (
+        <TextField
+          label="Name"
+          value={vectors[index].name}
+          onChange={(e) => handleVectorChange(index, 'name', e.target.value)}
+          error={!!errors[index].name}
+          helperText={errors[index].name}
+          sx={{ mr: 2 }}
+        />
+      )}
+      <FormControlLabel
+        control={
+          <Checkbox
+            color="primary"
+            onChange={(e) => {
+              handleVectorConfigChange(index, 'multivector_config', e.target.checked);
+            }}
+            checked={!!vectors[index].multivector_config}
+          />
+        }
+        label="Mutivector"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            color="primary"
+            onChange={(e) => {
+              handleVectorConfigChange(index, 'sparse_vectors', e.target.checked);
+            }}
+            checked={!!vectors[index].sparse_vectors}
+          />
+        }
+        label="Sparse Vectors"
+        labelPlacement="start"
+      />
+
+      {length > 1 && (
+        <Tooltip title="Remove vector" placement="top">
+          <IconButton onClick={() => handleRemoveVector(index)} color="error">
+            <DeleteOutline />
+          </IconButton>
+        </Tooltip>
+      )}
       {length - 1 === index && (
-        <Tooltip title="Add vector" placement="top">
+        <Tooltip title="Add new vector" placement="top">
           <IconButton onClick={handleAddVector} color="success">
             <AddCircleOutline />
           </IconButton>
@@ -117,16 +181,22 @@ const VectorConfig = ({ handleNext, handleBack, vectors, setVectors }) => {
   const validateAll = () => {
     const newErrors = vectors.map((vector) => {
       const error = {};
-      if (!vector.dimension) {
-        error.dimension = 'Dimension is required';
-      } else if (isNaN(vector.dimension)) {
-        error.dimension = 'Dimension must be a number';
-      }
-      if (!vector.distance) {
-        error.distance = 'Distance is required';
-      }
-      if (vectors.length > 1 && !vector.name) {
-        error.name = 'Vector name is required';
+      if (!vector.sparse_vectors) {
+        if (!vector.dimension) {
+          error.dimension = 'Dimension is required';
+        } else if (isNaN(vector.dimension)) {
+          error.dimension = 'Dimension must be a number';
+        }
+        if (!vector.distance) {
+          error.distance = 'Distance is required';
+        }
+        if (vectors.length > 1 && !vector.name) {
+          error.name = 'Vector name is required';
+        }
+      } else {
+        if (!vector.name) {
+          error.name = 'Vector name is required';
+        }
       }
       return error;
     });
