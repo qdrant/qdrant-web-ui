ccfbc4f92686d7dd7fb1817f10866e02d28d0734
David Myriel <davidmyriel@gmail.com>
2024-10-04 08:35:53 -0700

    [Docs] Add Tutorials (#231)
    * add tutorial incomplete
    
    * update multitenancy
    
    * Update src/components/InteractiveTutorial/TutorialSubpages.jsx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * add more text
    
    * update filtering text
    
    * add more tutorials
    
    * add load content tutorial
    
    * advanced filtering
    
    * add filtering examples
    
    * add full text filtering
    
    * add tutorials
    
    * Update src/components/InteractiveTutorial/MdxPages/FilteringAdvanced.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/FilteringAdvanced.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/FilteringBeginner.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/FilteringFullText.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/Multitenancy.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/Multitenancy.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/Multivectors.mdx
    
    Co-authored-by: Anush  <anushshetty90@gmail.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/HybridSearch.mdx
    
    Co-authored-by: Kacper Łukawski <kacperlukawski@users.noreply.github.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/HybridSearch.mdx
    
    Co-authored-by: Kacper Łukawski <kacperlukawski@users.noreply.github.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/HybridSearch.mdx
    
    Co-authored-by: Kacper Łukawski <kacperlukawski@users.noreply.github.com>
    
    * Update src/components/InteractiveTutorial/MdxPages/SparseVectors.mdx
    
    Co-authored-by: Kacper Łukawski <kacperlukawski@users.noreply.github.com>
    
    * update tutorials
    
    * update tutorials
    
    ---------
    
    Co-authored-by: Anush <anushshetty90@gmail.com>
    Co-authored-by: Kacper Łukawski <kacperlukawski@users.noreply.github.com>

--- src/components/InteractiveTutorial/TutorialSubpages.jsx
+++ src/components/InteractiveTutorial/TutorialSubpages.jsx
@@ -1,7 +1,13 @@
 import * as Index from './MdxPages/Index.mdx';
 import * as Quickstart from './MdxPages/Quickstart.mdx';
-import * as FilteringClauses from './MdxPages/FilteringClauses.mdx';
-
+import * as FilteringBeginner from './MdxPages/FilteringBeginner.mdx';
+import * as FilteringAdvanced from './MdxPages/FilteringAdvanced.mdx';
+import * as FilteringFullText from './MdxPages/FilteringFullText.mdx';
+import * as Multivectors from './MdxPages/Multivectors.mdx';
+import * as SparseVectors from './MdxPages/SparseVectors.mdx';
+import * as HybridSearch from './MdxPages/HybridSearch.mdx';
+import * as Multitenancy from './MdxPages/Multitenancy.mdx';
+import * as LoadContent from './MdxPages/LoadContent.mdx';
 /**
  * MDX page object (Index etc.) contains:
  *  - default: React component
@@ -12,7 +18,14 @@ import * as FilteringClauses from './MdxPages/FilteringClauses.mdx';
 export const tutorialIndexPage = Index;
 const tutorialSubPages = [
   ['quickstart', Quickstart],
-  ['filtering-clauses', FilteringClauses],
+  ['filteringbeginner', FilteringBeginner],
+  ['filteringadvanced', FilteringAdvanced],
+  ['filteringfulltext', FilteringFullText],
+  ['multivectors', Multivectors],
+  ['sparsevectors', SparseVectors],
+  ['hybridsearch', HybridSearch],
+  ['multitenancy', Multitenancy],
+  ['loadcontent', LoadContent],
   // add more pages here
 ];
 
