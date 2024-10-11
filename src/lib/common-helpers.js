da9ae9dc111f6af80e034579a17ba4250b532ac9
trean <trean.mi@gmail.com>
2024-10-11 18:52:21 +0200

    check version before displaying datasets

--- src/lib/common-helpers.js
+++ src/lib/common-helpers.js
@@ -7,3 +7,27 @@ export const resizeObserverWithCallback = (callback) => {
     }
   });
 };
+
+/**
+ * Compare two semver strings.
+ * @param {string} version1 - The first version string.
+ * @param {string} version2 - The second version string.
+ * @return {number} - Returns 0 if the versions are equal, 1 if version1 is greater, and -1 if version2 is greater.
+ */
+export const compareSemver = function(version1, version2) {
+  const parseVersion = (version) => version.split('.').map(Number);
+
+  const [major1, minor1, patch1] = parseVersion(version1);
+  const [major2, minor2, patch2] = parseVersion(version2);
+
+  if (major1 !== major2) {
+    return major1 > major2 ? 1 : -1;
+  }
+  if (minor1 !== minor2) {
+    return minor1 > minor2 ? 1 : -1;
+  }
+  if (patch1 !== patch2) {
+    return patch1 > patch2 ? 1 : -1;
+  }
+  return 0;
+}
--- src/pages/Datasets.jsx
+++ src/pages/Datasets.jsx
@@ -7,6 +7,7 @@ import { DatasetsTableRow } from '../components/Datasets/DatasetsTableRow';
 import { useClient } from '../context/client-context';
 import { useSnackbar } from 'notistack';
 import { getSnackbarOptions } from '../components/Common/utils/snackbarOptions';
+import { compareSemver } from "../lib/common-helpers";
 
 function Datasets() {
   const [datasets, setDatasets] = useState([]);
@@ -14,14 +15,23 @@ function Datasets() {
   const [isLoading, setIsLoading] = useState(false);
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const errorSnackbarOptions = getSnackbarOptions('error', closeSnackbar);
+  const {client} = useClient();
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
+        const telemetry = await client.api('service').telemetry();
         const response = await fetch('https://snapshots.qdrant.io/manifest.json');
         const responseJson = await response.json();
-        const datasets = responseJson.map((dataset) => {
+        const datasets = responseJson
+        .filter((dataset) => {
+          if (dataset.version === undefined) {
+            return true;
+          }
+          return compareSemver(telemetry.data.result.app.version, dataset.version) >= 0
+        })
+        .map((dataset) => {
           return {
             name: dataset.name,
             fileName: dataset.file_name,
