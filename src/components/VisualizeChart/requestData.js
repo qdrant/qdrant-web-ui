4007cd718c99be2530d66854c5a7c4e8e9945f2a
kartik-gupta-ij <kartikgupta7267@gmail.com>
2024-10-21 17:54:20 +0530

    fmt

--- src/components/VisualizeChart/requestData.js
+++ src/components/VisualizeChart/requestData.js
@@ -1,9 +1,5 @@
 /* eslint-disable camelcase */
-export function requestData(
-  qdrantClient,
-  collectionName,
-  { limit, filter = null, using = null, color_by = null }
-) {
+export function requestData(qdrantClient, collectionName, { limit, filter = null, using = null, color_by = null }) {
   // Based on the input parameters, we need to decide what kind of request we need to send
   // By default we should do scroll request
   // But if we have color_by field which uses query, it should be used instead
