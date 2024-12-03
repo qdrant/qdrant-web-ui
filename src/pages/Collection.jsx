c14fd88a0b2127c880e9f29bce494d1d4b29fdf9
kartik-gupta-ij <kartikgupta7267@gmail.com>
2024-12-03 22:37:04 +0530

    fix[visualize-tab] : disable visalize tab for sparse and multi vector on collections and view-point page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Typography, Grid, Tabs, Tab } from '@mui/material';
 import { CenteredFrame } from '../components/Common/CenteredFrame';
@@ -7,12 +7,32 @@ import { SnapshotsTab } from '../components/Snapshots/SnapshotsTab';
 import CollectionInfo from '../components/Collections/CollectionInfo';
 import PointsTabs from '../components/Points/PointsTabs';
 import SearchQuality from '../components/Collections/SearchQuality/SearchQuality';
+import { useClient } from '../context/client-context';
 
 function Collection() {
   const { collectionName } = useParams();
+  const { client: qdrantClient } = useClient();
   const navigate = useNavigate();
   const location = useLocation();
   const [currentTab, setCurrentTab] = useState(location.hash.slice(1) || 'points');
+  const [isDense, setIsDense] = useState(false);
+
+  useEffect(() => {
+    const checkDenseView = async () => {
+      const collectionData = await qdrantClient.getCollection(collectionName);
+      const vectors = collectionData.config.params.vectors;
+      if (vectors && vectors.size && vectors.distance && !vectors.multivector_config) {
+        setIsDense(true);
+        return;
+      }
+      const vectorNames = Object.keys(vectors);
+      const dense = vectorNames.some((vectorName) => {
+        return vectors[vectorName].size && vectors[vectorName].distance && !vectors[vectorName].multivector_config;
+      });
+      setIsDense(dense);
+    };
+    checkDenseView();
+  }, [collectionName, qdrantClient]);
 
   const handleTabChange = (event, newValue) => {
     if (typeof newValue !== 'string') {
@@ -36,7 +56,12 @@ function Collection() {
                 <Tab label="Info" value={'info'} />
                 <Tab label="Search Quality" value={'quality'} />
                 <Tab label="Snapshots" value={'snapshots'} />
-                <Tab label="Visualize" component={Link} to={`${location.pathname}/visualize`} />
+                <Tab
+                  label="Visualize"
+                  component={Link}
+                  to={`${location.pathname}/visualize`}
+                  disabled={!isDense}
+                />
                 <Tab label="Graph" component={Link} to={`${location.pathname}/graph`} />
               </Tabs>
             </Box>
