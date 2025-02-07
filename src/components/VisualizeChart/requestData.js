b0563f9ceea2fdf5da60c5371be0b4d8a31ab4d5
Kartik Gupta <84975264+kartik-gupta-ij@users.noreply.github.com>
2025-02-08 00:18:53 +0530

    chore[visualize-tab]: fix inconsistency in Visualize tab and add documentation (#248)
    * Fix inconsistency: Use `using` parameter in Visualize tab to match other tabs
    
    * docs [visualize-tab]:  add explanation for color_by.query the Visualize tab
    
    * fmt
    
    * fix[visualize-tab]: add using parameter in query object for visualization tab
    
    - fix color_by parameter in query object if dataset had multiple named vectors for visualization tab
    
    * fix[visualize-tab]: add support nested object path in `color_by` parameter for visualiztion tab
    
    fixes : #251
    
    * fix[visualize-tab]: refactor example query for visualiztion tab
    
    * fix[VisualizeChart]: define color for null and undefined in VisualizeChart (#260)

--- src/components/VisualizeChart/requestData.js
+++ src/components/VisualizeChart/requestData.js
@@ -1,9 +1,5 @@
 /* eslint-disable camelcase */
-export function requestData(
-  qdrantClient,
-  collectionName,
-  { limit, filter = null, vector_name = null, color_by = null }
-) {
+export function requestData(qdrantClient, collectionName, { limit, filter = null, using = null, color_by = null }) {
   // Based on the input parameters, we need to decide what kind of request we need to send
   // By default we should do scroll request
   // But if we have color_by field which uses query, it should be used instead
@@ -13,8 +9,9 @@ export function requestData(
       query: color_by.query,
       limit: limit,
       filter: filter,
-      with_vector: vector_name ? [vector_name] : true,
+      with_vector: using ? [using] : true,
       with_payload: true,
+      using: using ?? null,
     };
 
     return qdrantClient.query(collectionName, query);
@@ -25,7 +22,7 @@ export function requestData(
   const scrollQuery = {
     limit: limit,
     filter: filter,
-    with_vector: vector_name ? [vector_name] : true,
+    with_vector: using ? [using] : true,
     with_payload: true,
   };
 
