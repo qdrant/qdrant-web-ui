6d0b1c0d6db6a8fffa95576c474d246b0dddc517
trean <trean.mi@gmail.com>
2025-08-14 19:54:09 +0200

    colors

---
+++ src/theme/colors/deep-purple.js
@@ -0,0 +1,18 @@
+const deepPurple = {
+  50: '#ede7f6ff',
+  100: '#d1c4e9ff',
+  200: '#b39ddbff',
+  300: '#9575cdff',
+  400: '#7e57c2ff',
+  500: '#673ab7ff',
+  600: '#5e35b1ff',
+  700: '#512da8ff',
+  800: '#4527a0ff',
+  900: '#311b92ff',
+  A100: '#b388ffff',
+  A200: '#7c4dffff',
+  A400: '#651fffff',
+  A700: '#6200eaff',
+};
+
+export default deepPurple;
