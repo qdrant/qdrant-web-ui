7f8dcac003dffcf88b10c85a5d72c880842a51b5
trean <trean.mi@gmail.com>
2025-08-14 19:54:09 +0200

    colors

---
+++ src/theme/colors/green.js
@@ -0,0 +1,18 @@
+const green = {
+  50: '#e8f5e9ff',
+  100: '#c8e6c9ff',
+  200: '#a5d6a7ff',
+  300: '#81c784ff',
+  400: '#66bb6aff',
+  500: '#4caf50ff',
+  600: '#43a047ff',
+  700: '#388e3cff',
+  800: '#2e7d32ff',
+  900: '#1b5e20ff',
+  A100: '#b9f6caff',
+  A200: '#69f0aeff',
+  A400: '#00e676ff',
+  A700: '#00c853ff',
+};
+
+export default green;
