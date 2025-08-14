88f1f8006c624b012bebf17a8278144269063629
trean <trean.mi@gmail.com>
2025-08-14 20:10:34 +0200

    MUI theme colors update

--- src/theme/dark-theme.js
+++ src/theme/dark-theme.js
@@ -1,8 +1,5 @@
 import { alpha } from '@mui/material';
-
-const background = {
-  paper: '#252525',
-};
+import { primary, blue, red, orange, teal, neutral, lightBlue } from './colors';
 
 const getVariant = ({ theme, ownerState }) => {
   // dual cards have a white background and a 1px border around them
@@ -11,7 +8,7 @@ const getVariant = ({ theme, ownerState }) => {
   // (alike variant="elevation" elevation={1} cards)
   if (ownerState?.variant === 'dual') {
     return {
-      backgroundColor: background.paper,
+      backgroundColor: theme.palette.background.paper,
     };
   }
   if (ownerState?.variant === 'heading') {
@@ -24,10 +21,66 @@ const getVariant = ({ theme, ownerState }) => {
 // these options override the base dark theme
 export const darkThemeOptions = {
   palette: {
-    background: {
-      code: '#1e1e1e',
-      card: background.paper,
+    common: {
+      black: '#000000ff',
+      white: '#ffffffff',
     },
+    text: {
+      primary: neutral[1],
+      secondary: neutral[400],
+      disabled: neutral[700],
+    },
+    primary: {
+      main: primary[300],
+      dark: primary[500],
+      light: primary[200],
+      contrastText: neutral[950],
+    },
+    secondary: {
+      main: blue[300],
+      dark: blue[700],
+      light: blue[100],
+      contrastText: neutral[950],
+    },
+    error: {
+      main: red[500],
+      dark: red[700],
+      light: red[300],
+      contrastText: neutral[1],
+    },
+    warning: {
+      main: orange[400],
+      dark: orange[700],
+      light: orange[300],
+      contrastText: neutral[950],
+    },
+    info: {
+      main: lightBlue[400],
+      dark: lightBlue[700],
+      light: lightBlue[300],
+      contrastText: neutral[950],
+    },
+    success: {
+      main: teal[400],
+      dark: teal[700],
+      light: teal[300],
+      contrastText: neutral[950],
+    },
+    action: {
+      active: '#fcfdff8f',
+      hover: '#fcfdff14',
+      selected: '#fcfdff29',
+      focus: '#fcfdff1f',
+      disabled: '#fcfdff61',
+      disabledBackground: '#fcfdff1f',
+    },
+    background: {
+      default: neutral[950],
+      paper: '#111824ff',
+      card: '#111824ff',
+      code: '#1e1e1e',
+    },
+    divider: '#ffffff1f',
   },
   components: {
     MuiCard: {
