704e6b8c558410e43c758f42c50bb23497ef9dfd
trean <trean.mi@gmail.com>
2025-08-25 21:17:15 +0200

    format

--- src/theme/json-viewer-theme.js
+++ src/theme/json-viewer-theme.js
@@ -8,27 +8,27 @@ import { red, blue, neutral, yellow } from './colors';
  */
 export const createJsonViewerTheme = (theme) => {
   const isDark = theme.palette.mode === 'dark';
-  
+
   return {
     theme: {
-    scheme: 'qdrant-custom',
-    author: 'Qdrant Web UI',
-    base00: theme.palette.background.paperElevation1, // Default Background
-    // base01: isDark ? neutral[800] : neutral[100],    // Lighter Background (unused)
-    base02: isDark ? neutral[700] : neutral[200], // Borders and Background for types NaN, null, and undefined
-    // base03: isDark ? neutral[600] : neutral[300],    // Comments, Invisibles, Line Highlighting (unused)
-    base04: isDark ? neutral[500] : neutral[400], // Item Size
-    base05: red[600], // Default Foreground, Brackets, and Colons
-    base06: isDark ? neutral[300] : neutral[600], // Light Foreground (unused)
-    base07: red[600], // Keys, Colons, and Brackets
-    base08: theme.palette.error.main, // Color for NaN
-    base09: blue[600], // Ellipsis and String Values
-    base0A: theme.palette.warning.main, // Regular Expressions and Null Values
-    base0B: blue[600], // Floating-Point Values
-    base0C: red[600], // Number Keys
-    base0D: isDark ? neutral[500] : neutral[400], // Icons, Search Input, Date
-    base0E: theme.palette.warning.main, // Booleans and Expanded Icons
-    base0F: blue[600], // Integers
+      scheme: 'qdrant-custom',
+      author: 'Qdrant Web UI',
+      base00: theme.palette.background.paperElevation1, // Default Background
+      // base01: isDark ? neutral[800] : neutral[100],    // Lighter Background (unused)
+      base02: isDark ? neutral[700] : neutral[200], // Borders and Background for types NaN, null, and undefined
+      // base03: isDark ? neutral[600] : neutral[300],    // Comments, Invisibles, Line Highlighting (unused)
+      base04: isDark ? neutral[500] : neutral[400], // Item Size
+      base05: red[600], // Default Foreground, Brackets, and Colons
+      base06: isDark ? neutral[300] : neutral[600], // Light Foreground (unused)
+      base07: red[600], // Keys, Colons, and Brackets
+      base08: theme.palette.error.main, // Color for NaN
+      base09: blue[600], // Ellipsis and String Values
+      base0A: theme.palette.warning.main, // Regular Expressions and Null Values
+      base0B: blue[600], // Floating-Point Values
+      base0C: red[600], // Number Keys
+      base0D: isDark ? neutral[500] : neutral[400], // Icons, Search Input, Date
+      base0E: theme.palette.warning.main, // Booleans and Expanded Icons
+      base0F: blue[600], // Integers
     },
     overrides: {
       fontSize: '0.75rem',
