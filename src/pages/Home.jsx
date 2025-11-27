3b58b9d35e0dc8726d46b8d0587aebbbac50c137
trean <trean.mi@gmail.com>
2025-11-27 23:58:22 +0100

    iframe layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,12 @@ function HomeContent() {
   const { authError, clearAuthError } = useAuthError();
   const { cloudInfo } = useCloudInfo();
 
+  const [isInIframe, setIsInIframe] = useState(false);
   const [apiKeyDialogOpen, setApiKeyDialogOpen] = useState(false);
+
+  useEffect(() => {
+    setIsInIframe(window.self !== window.top);
+  }, []);
 
   useEffect(() => {
     if (authError) {
@@ -49,63 +54,65 @@ function HomeContent() {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          zIndex: (theme) => theme.zIndex.drawer,
-          background: theme.palette.background.paper,
-          boxShadow: 'none',
-          borderBottom: `1px solid ${theme.palette.divider}`,
-        }}
-      >
-        <Toolbar>
-          <Logo width={200} />
-          {cloudInfo?.cluster_name ? (
-            <Box sx={{ flexGrow: 1, pl: '140px', display: 'flex', alignItems: 'center', gap: 1 }}>
-              <Typography variant="body1" sx={{ color: theme.palette.text.primary }}>
-                cluster
-              </Typography>
-              <Typography variant="body1" sx={{ color: theme.palette.text.primary }}>
-                /
-              </Typography>
-              <Typography
-                component={Link}
-                to={cloudInfo.cloud_backlink}
-                variant="body1"
-                sx={{
-                  color: theme.palette.text.primary,
-                  fontWeight: 500,
-                  textDecoration: 'none',
-                  '&:hover': {
-                    textDecoration: 'underline',
-                    textDecorationThickness: '1px',
-                    textUnderlineOffset: '2px',
-                  },
-                }}
-              >
-                {cloudInfo.cluster_name}
-              </Typography>
-            </Box>
-          ) : (
-            <Box sx={{ flexGrow: 1 }}></Box>
-          )}
+      {!isInIframe ? (
+        <>
+          <AppBar
+            position="fixed"
+            sx={{
+              zIndex: (theme) => theme.zIndex.drawer,
+              background: theme.palette.background.paper,
+              boxShadow: 'none',
+              borderBottom: `1px solid ${theme.palette.divider}`,
+            }}
+          >
+            <Toolbar>
+              <Logo width={200} />
+              {cloudInfo?.cluster_name ? (
+                <Box sx={{ flexGrow: 1, pl: '140px', display: 'flex', alignItems: 'center', gap: 1 }}>
+                  <Typography variant="body1" sx={{ color: theme.palette.text.primary }}>
+                    cluster
+                  </Typography>
+                  <Typography variant="body1" sx={{ color: theme.palette.text.primary }}>
+                    /
+                  </Typography>
+                  <Typography
+                    component={Link}
+                    to={cloudInfo.cloud_backlink}
+                    variant="body1"
+                    sx={{
+                      color: theme.palette.text.primary,
+                      fontWeight: 500,
+                      textDecoration: 'none',
+                      '&:hover': {
+                        textDecoration: 'underline',
+                        textDecorationThickness: '1px',
+                        textUnderlineOffset: '2px',
+                      },
+                    }}
+                  >
+                    {cloudInfo.cluster_name}
+                  </Typography>
+                </Box>
+              ) : (
+                <Box sx={{ flexGrow: 1 }}></Box>
+              )}
 
-          {cloudInfo?.scale_url && (
-            <Button
-              component={Link}
-              to={cloudInfo.scale_url}
-              target="_blank"
-              variant="contained"
-              color="primary"
-              size="small"
-              endIcon={<Rocket size={16} />}
-              sx={{ mr: 2 }}
-            >
-              Upgrade Cluster
-            </Button>
-          )}
+              {cloudInfo?.scale_url && (
+                <Button
+                  component={Link}
+                  to={cloudInfo.scale_url}
+                  target="_blank"
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  endIcon={<Rocket size={16} />}
+                  sx={{ mr: 2 }}
+                >
+                  Upgrade Cluster
+                </Button>
+              )}
 
-          {/* <Button
+              {/* <Button
             component={Link}
             to="https://qdrant.tech/cloud/" // todo: replace with the actual link
             target="_blank"
@@ -118,19 +125,23 @@ function HomeContent() {
           >
             Get Managed Cloud
           </Button> */}
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-            <Tooltip title="API Key">
-              <IconButton size="large" onClick={() => setApiKeyDialogOpen(true)}>
-                <Key size={20} />
-              </IconButton>
-            </Tooltip>
-            <ColorModeToggle />
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Sidebar />
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <Tooltip title="API Key">
+                  <IconButton size="large" onClick={() => setApiKeyDialogOpen(true)}>
+                    <Key size={20} />
+                  </IconButton>
+                </Tooltip>
+                <ColorModeToggle />
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Sidebar />
+        </>
+      ) : (
+        <></>
+      )}
       <Box component="main" sx={{ flexGrow: 1, overflow: 'hidden' }}>
-        <DrawerHeader />
+        {!isInIframe ? <DrawerHeader /> : <></>}
         <Outlet />
       </Box>
       <ApiKeyDialog open={apiKeyDialogOpen} setOpen={handleDialogClose} onApply={OnApyKeyApply} />
