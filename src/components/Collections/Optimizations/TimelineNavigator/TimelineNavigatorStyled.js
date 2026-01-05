c5cf4b95249b790988db443a3ed770e89343c526
trean <trean.mi@gmail.com>
2026-01-05 18:48:33 +0100

    white thumbs dots in the light theme

--- src/components/Collections/Optimizations/TimelineNavigator/TimelineNavigatorStyled.js
+++ src/components/Collections/Optimizations/TimelineNavigator/TimelineNavigatorStyled.js
@@ -41,9 +41,9 @@ export const RangeSlider = styled(Slider)(({ theme }) => ({
     width: 8,
     height: NAVIGATOR_HEIGHT - 24,
     borderRadius: 4,
-    backgroundColor: theme.palette.primary.dark,
+    backgroundColor: theme.palette.mode === 'light' ? theme.palette.primary.light : theme.palette.primary.dark,
     backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='24' fill='${encodeURIComponent(
-      theme.palette.text.primary
+      theme.palette.mode === 'light' ? '#ffffff' : theme.palette.text.primary
     )}' viewBox='0 0 24 24'%3E%3Cpath d='M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z' /%3E%3C/svg%3E")`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
