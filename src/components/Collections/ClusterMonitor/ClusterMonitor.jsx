4fcce369798b723ef5a96a33de6d4a7ab6bb9c81
trean <trean.mi@gmail.com>
2026-02-19 10:23:33 +0100

    Fix cluster monitor arrows (#352)
    * fix target node selection for the arrow
    
    * fix arrows rendering for pointing in the same peer
    
    * format
    
    * audit fix

--- src/components/Collections/ClusterMonitor/ClusterMonitor.jsx
+++ src/components/Collections/ClusterMonitor/ClusterMonitor.jsx
@@ -444,7 +444,10 @@ const ClusterMonitor = ({ collectionName }) => {
             width: 'max-content',
           }}
         >
-          <ArcherContainer strokeColor={theme.palette.mode === 'dark' ? '#d4d9e6' : '#647cb9'} lineStyle={'angle'}>
+          <ArcherContainer
+            strokeColor={theme.palette.mode === 'dark' ? theme.palette.primary.light : theme.palette.primary.main}
+            lineStyle={'angle'}
+          >
             <Grid container spacing={2}>
               {cluster.peers?.map((peerId) => (
                 <Grid key={peerId} size={'grow'}>
