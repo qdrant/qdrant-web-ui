826b67d2aeb7a88ffe8dbbf3d37f1b278262380e
Srimon <srimon12mckv@gmail.com>
2026-05-15 06:54:18 +0530

    Preserve full error toast messages

--- src/components/ToastNotifications/ErrorNotifier.jsx
+++ src/components/ToastNotifications/ErrorNotifier.jsx
@@ -1,27 +1,16 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSnackbar } from 'notistack';
 import { getSnackbarOptions } from '../Common/utils/snackbarOptions';
-
-const MAX_MESSAGE_LENGTH = 250;
-
-const truncateMessage = (msg) => {
-  if (msg && msg.length > MAX_MESSAGE_LENGTH) {
-    return msg.substring(0, MAX_MESSAGE_LENGTH - 3) + '...';
-  }
-  return msg;
-};
 
 export const ErrorNotifier = ({ message = 'Something went wrong', callback }) => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const errorSnackbarOptions = getSnackbarOptions('error', closeSnackbar, 6000);
 
-  const truncatedMessage = useMemo(() => truncateMessage(message), [message]);
-
   useEffect(() => {
-    enqueueSnackbar(truncatedMessage, errorSnackbarOptions);
+    enqueueSnackbar(message, errorSnackbarOptions);
     typeof callback === 'function' && callback();
-  }, [enqueueSnackbar, errorSnackbarOptions, truncatedMessage]);
+  }, [enqueueSnackbar, errorSnackbarOptions, message, callback]);
 
   return null;
 };
