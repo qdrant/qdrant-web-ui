94d6aae65c0759ff113215a9611570dd83ff6d41
trean <trean.mi@gmail.com>
2026-07-04 10:29:43 +0200

    Fix buttons tooltips (#403)
    * remove redundant tooltips on the create collection button and on the upload snapshot button
    
    * format

--- src/components/Collections/CreateCollection/CreateCollectionButton.jsx
+++ src/components/Collections/CreateCollection/CreateCollectionButton.jsx
@@ -5,6 +5,8 @@ import AddIcon from '@mui/icons-material/Add';
 import { useClient } from '../../../context/client-context';
 import PropTypes from 'prop-types';
 
+const BUTTON_LABEL = 'Create Collection';
+
 const CreateCollectionButton = ({ onComplete }) => {
   const [open, setOpen] = useState(false);
   const { isRestricted } = useClient();
@@ -22,27 +24,34 @@ const CreateCollectionButton = ({ onComplete }) => {
 
   return (
     <>
-      <Tooltip
-        title={
-          isRestricted
-            ? 'Access Denied: You do not have permission to create collections. Please contact your administrator.'
-            : 'Create collection'
-        }
-        placement="left"
-      >
-        {/* span is needed to allow tooltip on disabled button */}
-        <span>
-          <Button
-            variant="contained"
-            startIcon={<AddIcon fontSize="small" />}
-            onClick={handleOpen}
-            disabled={isRestricted}
-            aria-label="Create Collection"
-          >
-            Create Collection
-          </Button>
-        </span>
-      </Tooltip>
+      {isRestricted ? (
+        <Tooltip
+          title="Access Denied: You do not have permission to create collections. Please contact your administrator."
+          placement="bottom"
+        >
+          {/* span is needed to allow tooltip on disabled button */}
+          <span>
+            <Button
+              variant="contained"
+              startIcon={<AddIcon fontSize="small" />}
+              onClick={handleOpen}
+              disabled
+              aria-label={BUTTON_LABEL}
+            >
+              {BUTTON_LABEL}
+            </Button>
+          </span>
+        </Tooltip>
+      ) : (
+        <Button
+          variant="contained"
+          startIcon={<AddIcon fontSize="small" />}
+          onClick={handleOpen}
+          aria-label={BUTTON_LABEL}
+        >
+          {BUTTON_LABEL}
+        </Button>
+      )}
       <CreateCollectionDialog open={open} handleClose={handleClose} />
     </>
   );
